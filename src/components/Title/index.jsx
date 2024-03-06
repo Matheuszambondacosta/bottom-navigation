fd3575043e82dc7b4242dd5d699b4e90ad3d6aab
Matheszambondacosta <matheus-costa@aluno.senai.br>
2024-03-06 08:17:00 -0300

    Atualizar estilos de título e home

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import styles from './style'
 
 const Title = () => {
   return (
-    <View>
-      <Text>Title</Text>
+    <View style={styles.container}>
+      <Text style={styles.container}>Title</Text>
     </View>
   )
 }
